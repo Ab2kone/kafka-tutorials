e77949d48311efbbd0500ed1faafbf719df0a030
Aboubakar <ab2kone@gmail.com>
2021-08-03 22:54:36 +0000

    Consumer with thread

--- src/main/java/com/github/abk/tuto1/ConsumerDemoGroups.java
+++ src/main/java/com/github/abk/tuto1/ConsumerDemoGroups.java
@@ -18,13 +18,13 @@ public class ConsumerDemoGroups {
         Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class);
 
         String bootstrapServers = "127.0.0.1:9092";
-        String groupId = "my-first-application";
+        String groupId = "my-third-application";
         String topic = "first_topic";
 
 
         // Create consumer config
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
